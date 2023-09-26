79ca18502d269f1a75ca851ab972b3b5b572561c
DilshadNirmal <dilshadgov2020@gmail.com>
2023-09-26 11:39:01 +0530

    js completed

---
+++ JavaScript/12_tabs/script.js
@@ -0,0 +1,10 @@
+/*
+ * - Add an event listener for document click
+ * - Define a function that filters the unwanted click events on the document
+ */
+
+// Add an event listener for document click
+
+
+// Define a function that filters the unwanted click events on the document
+
