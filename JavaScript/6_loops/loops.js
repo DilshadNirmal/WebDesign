643c5e77aeedbe2cfcf88ed58a8b547f075a302a
DilshadNirmal <dilshadgov2020@gmail.com>
2023-10-25 13:08:02 +0530

    till functions

--- JavaScript/6_loops/loops.js
+++ JavaScript/6_loops/loops.js
@@ -4,15 +4,27 @@
 //     console.log(value);
 // }
 
-let value = 2
+// let value = 2
 
 // while(value < 2) {
 //     console.log(value);
 //     value++;
 // }
 
-do {
-    console.log(value);
-    value++;
-    console.log(value);
-} while(value < 2);
+// do {
+//     console.log(value);
+//     value++;
+//     console.log(value);
+// } while(value < 2);
+
+revanth_TenthMarks = [78, 45, 99, 22, 55];
+
+// for (let index = 0; index < revanth_TenthMarks.length; index++) {
+//     console.log(revanth_TenthMarks[index]);
+// }
+
+// for of loop
+
+for (let revanth_marks of revanth_TenthMarks) {
+    console.log(revanth_marks);
+}
--- JavaScript/7_functions/functions.js
+++ JavaScript/7_functions/functions.js
@@ -1,6 +1,9 @@
 // this is redundant function
+// function print(value) {
+//     console.log(value);
+// }
 
-
+// print('revanth')
 
 /*
 *------------------------------------------------------------------
@@ -14,7 +17,49 @@
 *   function with return type and without argument
 */
 
+// function decalration
+// function add(a, b) {
+//     print(a + b)
+// }
+
+// function sub(a, b) {
+//     return a - b;
+// }
+
+// function withoutReturnType() {
+//     print('i am a function which has no parameter and no return type');
+// }
+
+// function withReturnType() {
+//     return 'i am a function which has no parameter but with return type';
+// }
+
+// function call
+// add(3, 6);
+// print(sub(45, 23));
+// withoutReturnType();
+// console.log(withReturnType());
+
+// function expression
 
 
-// functions inside a variable
 
+divide(3, 1);
+
+function divide(a, b) {
+    console.log(a / b);
+}
+
+let modulo = function(a, b) {
+    console.log(a % b);
+}
+
+modulo(3, 1);
+
+// arrow function
+
+let print = (value) => {
+    console.log(value);
+}
+
+print(45)
